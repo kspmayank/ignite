e4200b30f3f26376b1f30ba5023330b6cce62669
Mayank <mayankkashyap95@gmail.com>
2016-02-12 20:53:48 +0530

    Bug fixes

--- sendemail.php
+++ sendemail.php
@@ -5,16 +5,21 @@
 		'message'=>'Thank you for contacting us. We will contact you As early as possible  '
 	);
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+    // $name = @trim(stripslashes($_POST['name'])); 
+    // $email = @trim(stripslashes($_POST['email'])); 
+    // $subject = @trim(stripslashes($_POST['subject'])); 
+    // $message = @trim(stripslashes($_POST['message'])); 
+
+
+    $name = 'name'; 
+    $email = 'email'; 
+    $subject = 'subject'; 
+    $message = 'message'; 
 
     $email_from = $email;
     $email_to = 'mayankkashyap95@gmail.com';//replace with your email
 
-    // $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
-    $body = $name;
+    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
     $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
