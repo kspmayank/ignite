40f346fe1cbdc032f92cb70f5ad026ed22d75186
Mayank <mayankkashyap95@gmail.com>
2016-02-12 20:46:20 +0530

    Bug fixes

--- sendemail.php
+++ sendemail.php
@@ -13,8 +13,8 @@
     $email_from = $email;
     $email_to = 'mayankkashyap95@gmail.com';//replace with your email
 
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
-
+    // $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+    $body = $name;
     $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
     echo json_encode($status);
